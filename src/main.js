dc8490c38d943b8558b201cf97f4c811d482db09
笑对生活 <165551770@qq.com>
2019-10-06 10:37:56 +0800

    黑马买买商城首页

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import '../src/assets/site/css/style.css'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+//1.导入路由组件
+import index from './components/index.vue'
+//2.设置路由规则
+const routes = [
+  {path:'/index',component:index},
+  {path:'/',redirect:'/index'}
+]
+//3.实例化路由对象
+const router = new VueRouter({
+  routes
+
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
